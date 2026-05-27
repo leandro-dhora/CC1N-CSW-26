f7282a70f005f2727201c14a2e2083c4f0d0932c
Leandro da Hora <lsousa.dhora@gmail.com>
2026-05-27 20:48:48 -0300

    Add files via upload

---
+++ aula_17/script.js
@@ -0,0 +1,27 @@
+const botao = document.getElementById("calcular")
+const resultado = document.getElementById("resultado")
+
+botao.addEventListener("click", function () {
+    const dia = Number(document.getElementById("dia").value)
+    const mes = Number(document.getElementById("mes").value)
+    const ano = Number(document.getElementById("ano").value)
+
+    const hoje = new Date()
+
+    const anoAtual = hoje.getFullYear()
+    const mesAtual = hoje.getMonth() + 1
+    const diaAtual = hoje.getDate()
+
+    let idade = anoAtual - ano
+
+    if (mesAtual < mes || (mesAtual === mes && diaAtual < dia)) {
+        idade--
+    }
+
+    if (!dia || !mes || !ano) {
+        resultado.textContent = "Preencha todos os campos!"
+        return
+    }
+
+    resultado.textContent = `Você tem ${idade} anos.`
+})
